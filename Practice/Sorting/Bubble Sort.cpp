cb21677c7a4a1378cf2e016047101771f4096a3b
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-09-18 13:31:50 +0500

    Update Bubble Sort.cpp

--- Practice/Sorting/Bubble Sort.cpp
+++ Practice/Sorting/Bubble Sort.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+//Time Complexity of this sort is O(n^2)
+//it will also n^2 times on sorted array
 int main(){
 
     int arr[] = {4,1,5,2,3};
@@ -13,14 +15,22 @@ int main(){
 
 
     for(int i = 0; i < size; i++){
+        //isSwap = false;
         for(int j = 0; j < size-1; j++){
             if(arr[j] > arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp; 
+                //isSwap = false;
             }
         }
     }
+
+    //if(isSwap){
+        //cout << "The array is Sorted" << endl;
+        //return; 
+    //}
+    
 
     cout << endl;
     cout << "Bubble Sorted Array" << endl;
