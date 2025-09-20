55824b6c27a495acdf0f271d7d581ea0aa2c2d73
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-09-20 17:51:59 +0500

    Create Sample.cpp

---
+++ Practice/Jagged Arrays/Sample.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void jaggedArray(int rows){
+    int **Arows;
+    int *Acoloumns;
+
+    Acoloumns = new int[rows];
+    cout << "Set different Coloumns: ";
+    for(int i = 0; i < rows; i++){
+        cout << "For row: " << i+1;
+        cin >> coloumns[i]
+    }
+
+    Arows = new int*[rows];
+    for(int i = 0; i < rows; i++){
+        Arows[i] = new int(Acoloumns[i]);
+    }
+
+    
+}
+
+int main(){
+
+return 0;
+}
