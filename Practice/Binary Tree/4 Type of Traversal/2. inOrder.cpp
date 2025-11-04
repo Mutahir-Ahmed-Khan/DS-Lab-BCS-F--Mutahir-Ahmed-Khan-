bd2fcdb5c82a34725286bfb77b8844271accca98
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-11-04 21:29:52 +0500

    Update 2. inOrder.cpp

--- Practice/Binary Tree/4 Type of Traversal/2. inOrder.cpp
+++ Practice/Binary Tree/4 Type of Traversal/2. inOrder.cpp
@@ -1,1 +1,10 @@
+void inOrder(node* root){
+    //Sequence: left, root, right
+    if(root == NULL){
+        return;
+    }
 
+    inOrder(root->left);
+    cout << root->data << " "; 
+    inOrder(root->right);
+}
