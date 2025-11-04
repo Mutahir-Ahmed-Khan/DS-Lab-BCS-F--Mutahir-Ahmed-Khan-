473c8aed58c4730ee6fd90f580839655b51a9101
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-11-04 21:26:42 +0500

    Update 1. preOrder.cpp

--- Practice/Binary Tree/4 Type of Traversal/1. preOrder.cpp
+++ Practice/Binary Tree/4 Type of Traversal/1. preOrder.cpp
@@ -1,1 +1,10 @@
+void preOrder(node* root){
+    //Sequence: root, left, right
+    if(root == NULL){
+        return;
+    }
 
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
