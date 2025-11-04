ea147488e72801ca2aed31f7556f21eb7802ba6b
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-11-04 21:17:52 +0500

    Update building a tree.cpp

--- Practice/Binary Tree/building a tree.cpp
+++ Practice/Binary Tree/building a tree.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+class node{
+    public: 
+    int data;
+    node* left; 
+    node* right;
+
+    node(int val){
+        data = val; 
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+node* buildTree(int *arr){
+    idx++;
+
+     if(arr[idx] == -1){
+        return NULL;
+    }
+
+    node* root = new node(arr[idx]);
+    root->left = buildTree(arr);
+    root->right = buildTree(arr);
+
+    return root;
+}
+
+int main(){
+
+    int preOrder[11] = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    node* root = buildTree(preOrder); 
+    cout << root->right->data << endl;
+
+return 0;
+}
